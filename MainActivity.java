dd147228b558d0a917817558b95c86dca1af3bf6
nishikashah07 <139798137+nishikashah07@users.noreply.github.com>
2025-02-24 14:44:32 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.quickbitez;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ImageButton nextButton = findViewById(R.id.nextButton);
+
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish(); // Ensures the welcome screen is skipped after login
+            }
+        });
+    }
+}
